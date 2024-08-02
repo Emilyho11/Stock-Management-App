09ea39a2c018048678aaa1d2cd396ab9dbcdeba7
emmimelon <emmimelon@gmail.com>
2024-08-02 10:29:52 -0400

    chore: move functions around in portfolio

--- backend_old/src/Portfolio.java
+++ backend_old/src/Portfolio.java
@@ -55,25 +55,20 @@ public class Portfolio {
         }
     }
 
-    //adds portfolio to database and the owner/id tuple into the owns table
-    public static void createNewUserPortfolio(String owner, String name, Connection conn){
-        int newid = getNewId(conn);
-        if(newid == -1){
-            try {
-                createNewPortfolio(name, newid, conn);
-                PreparedStatement stmt;
-                stmt = conn.prepareStatement("INSERT INTO owns (username, portfolioId) VALUES (?, ?);");
-                stmt.setString(1, owner);
-                stmt.setInt(2, newid);
-                stmt.executeUpdate();
-                System.out.println("Created a new portfolio successfully for this user");
-            } catch (Exception ex) {
-                System.out.println("Error creating a new portfolio for this user" + ex.getMessage());
-            }
+    //check the portfolio db to see if this portfolio exists or not
+    private static boolean checkPortfolioExist(int id, Connection conn){
+        // Find the portfolio in the database by the id
+        try {
+            PreparedStatement stmt;
+            stmt = conn.prepareStatement("SELECT EXISTS(SELECT * FROM portfolio WHERE portfolioId = ?);");
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            System.out.println("Checked for this portfolio id successfully");
+            return rs.isBeforeFirst();
+        } catch (SQLException ex) {
+            System.out.println("Error finding this user's portfolio: " + ex.getMessage());
         }
-        else {
-            System.out.println("Error getting a new portfolio id");
-        }
+        return false;
     }
 
     private static void createNewPortfolio(String name, int id, Connection conn){
@@ -88,6 +83,27 @@ public class Portfolio {
             } catch (Exception ex) {
                 System.out.println("Error creating a new portfolio" + ex.getMessage());
             }
+        }
+    }
+
+    //adds portfolio to database and the owner/id tuple into the owns table
+    public static void createNewUserPortfolio(String owner, String name, Connection conn){
+        int newid = getNewId(conn);
+        if(newid == -1){
+            try {
+                Portfolio.createNewPortfolio(name, newid, conn);
+                PreparedStatement stmt;
+                stmt = conn.prepareStatement("INSERT INTO owns (username, portfolioId) VALUES (?, ?);");
+                stmt.setString(1, owner);
+                stmt.setInt(2, newid);
+                stmt.executeUpdate();
+                System.out.println("Created a new portfolio successfully for this user");
+            } catch (Exception ex) {
+                System.out.println("Error creating a new portfolio for this user" + ex.getMessage());
+            }
+        }
+        else {
+            System.out.println("Error getting a new portfolio id");
         }
     }
 
@@ -122,22 +138,6 @@ public class Portfolio {
             System.out.println("Error finding this user's portfolio: " + ex.getMessage());
         }
         return null;    
-    }
-
-    //check the portfolio db to see if this portfolio exists or not
-    private static boolean checkPortfolioExist(int id, Connection conn){
-        // Find the portfolio in the database by the id
-        try {
-            PreparedStatement stmt;
-            stmt = conn.prepareStatement("SELECT EXISTS(SELECT * FROM portfolio WHERE portfolioId = ?);");
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            System.out.println("Checked for this portfolio id successfully");
-            return rs.isBeforeFirst();
-        } catch (SQLException ex) {
-            System.out.println("Error finding this user's portfolio: " + ex.getMessage());
-        }
-        return false;
     }
 
     // public static StockList* findStockLists() {
