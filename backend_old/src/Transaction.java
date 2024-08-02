875349ac688107424f9575847513d642865e1f5c
emmimelon <emmimelon@gmail.com>
2024-08-02 12:38:28 -0400

    feat: added buy/sell transaction insertion queries

--- backend_old/src/Transaction.java
+++ backend_old/src/Transaction.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
+import cs.toronto.edu.src.StockData;
+
 public class Transaction {
 
     private int transactionId;
@@ -36,14 +38,52 @@ public class Transaction {
 
     //gets the latest stock price of a given stock
     private static double getCurrentStockValue(String symbol, Connection conn){
-        return -1;
+        ResultSet rs = StockData.getLatestStockData(symbol, conn);
+        try {
+            return rs.getDouble("close");
+        } catch (SQLException ex) {
+            return -1;
+        }
     }
 
-    public static void sellStock(int portfolioId, String symbol, int quantity){
-
+    //creates a new transaction of selling a stock
+    public static void sellStock(int portfolioId, String symbol, int quantity, Connection conn){
+        try {
+            int tid = getNewId(portfolioId, conn);
+            double stockprice = getCurrentStockValue(symbol, conn);
+            PreparedStatement stmt;
+            stmt = conn.prepareStatement("INSERT INTO transaction (transactionId, portfolioId, transactionType, transactionDate, symbol, price, quantity) VALUES (?, ?, 'sell', ?, ?, ?, ?);");
+            stmt.setInt(1, tid);
+            stmt.setInt(2, portfolioId);
+            Date today = new Date();
+            stmt.setDate(3, (java.sql.Date) today);
+            stmt.setString(4, symbol);
+            stmt.setDouble(5, stockprice);
+            stmt.setInt(6, quantity);
+            stmt.executeUpdate();
+            System.out.println("Created sell stock transaction successfully");
+        } catch (SQLException ex) {
+            System.out.println("Error creating sell stock transaction" + ex.getMessage());
+        }
     }
 
-    public static void buyStock(int portfolioId, String symbol, int quantity){
-
+    public static void buyStock(int portfolioId, String symbol, int quantity, Connection conn){
+        try {
+            int tid = getNewId(portfolioId, conn);
+            double stockprice = getCurrentStockValue(symbol, conn);
+            PreparedStatement stmt;
+            stmt = conn.prepareStatement("INSERT INTO transaction (transactionId, portfolioId, transactionType, transactionDate, symbol, price, quantity) VALUES (?, ?, 'buy', ?, ?, ?, ?);");
+            stmt.setInt(1, tid);
+            stmt.setInt(2, portfolioId);
+            Date today = new Date();
+            stmt.setDate(3, (java.sql.Date) today);
+            stmt.setString(4, symbol);
+            stmt.setDouble(5, stockprice);
+            stmt.setInt(6, quantity);
+            stmt.executeUpdate();
+            System.out.println("Created buy stock transaction successfully");
+        } catch (SQLException ex) {
+            System.out.println("Error creating buy stock transaction" + ex.getMessage());
+        }
     }
 }
