d6936398a72484805085382ca434551071fa49b2
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-08-02 13:50:10 -0400

    Modifying .sh files

--- backend_old/ActuallyRun.sh
+++ backend_old/ActuallyRun.sh
@@ -1,3 +1,4 @@
 rm -rf pgsample
+rm -rf ../backend/src/main/java/backend/logic/src
 ./SetupProject.sh
 ./BuildAndRun.sh
--- backend_old/SetupProject.sh
+++ backend_old/SetupProject.sh
@@ -2,4 +2,6 @@ mvn archetype:generate -DgroupId=cs.toronto.edu -DartifactId=pgsample -Darchetyp
 cp Main.xml pgsample/pom.xml
 cp Main.java pgsample/src/main/java/cs/toronto/edu/
 cp -r ./src pgsample/src/main/java/cs/toronto/edu/
+
+cp -r ./pgsample/src ../backend/src/main/java/backend/logic
 rm pgsample/src/main/java/cs/toronto/edu/App.java
