5e9244490b93155af0eea9a3f9a1e75637775230
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-08-03 21:11:39 -0400

    Fixing stockList apisa

--- stocks_api/src/main/java/stocks_api/stocks_api/routes/StockListController.java
+++ stocks_api/src/main/java/stocks_api/stocks_api/routes/StockListController.java
@@ -9,13 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import stocks_api.stocks_api.logic.src.DBHandler;
 import stocks_api.stocks_api.logic.src.ParserUtil;
 import stocks_api.stocks_api.logic.src.Portfolio;
 import stocks_api.stocks_api.logic.src.StockList;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/stocklist", produces="application/json")
 
