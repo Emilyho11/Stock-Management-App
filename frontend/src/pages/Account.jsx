9ce45a5320d74cba8864c29e206cec542fa2c0d7
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-08-06 19:17:56 -0400

    Modifying account.jsx to have delete button

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -4,6 +4,7 @@ import Button from "../components/Button";
 import { Link } from "react-router-dom";
 import { useAuth } from "../components/AuthContext";
 import { useNavigate } from "react-router-dom";
+import AxiosClient from "../api/AxiosClient";
 
 const Account = () => {
 	const { getUsername, isLoggedIn } = useAuth();
@@ -20,9 +21,23 @@ const Account = () => {
 		return <div>Redirecting to login...</div>;
 	}
 
+	// Delete account api
+	const deleteAccount = async () => {
+		try {
+			const response = await AxiosClient.delete("account/");
+			if (response.status === 204) {
+				navigate("/logout");
+			} else {
+				console.error("Unexpected response:", response);
+			}
+		} catch (error) {
+			console.error("Error deleting account:", error);
+		}
+	}
+
 	return (
 		<div className="mx-16 my-4">
-			<h1>Account</h1>
+			<h1>Account: {getUsername()}</h1>
 
 			<Card className="flex flex-col w-1/4">
 				<h2 className="text-xl">Transaction History</h2>
@@ -41,7 +56,7 @@ const Account = () => {
 			<Link to={"/logout"}>
 				<Button className="mt-4">Logout</Button>
 			</Link>
-			<p>Logged in as: {getUsername()}</p>
+			<Button className="bg-red-700">Delete Account</Button>
 		</div>
 	);
 };
