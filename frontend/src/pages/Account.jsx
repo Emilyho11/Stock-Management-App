c8db58f43f6e6b1194f551472e896f031e5051ac
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-08-06 14:38:29 -0400

    Modifying global access to user in my account page

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "../components/Card";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
 import { useAuth } from "../components/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Account = () => {
-	const { username } = useAuth();
-	console.log("username", username);
+	const { getUsername, isLoggedIn } = useAuth();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!isLoggedIn()) {
+			navigate("/login");
+			navigate(0);
+		}
+	}, []);
+
+	if (!isLoggedIn()) {
+		return <div>Redirecting to login...</div>;
+	}
 
 	return (
 		<div className="mx-16 my-4">
@@ -17,20 +29,19 @@ const Account = () => {
 				<div className="w-1/3">
 					<p className="text-base">Stocks Bought: 0</p>
 					<p className="text-base">Stocks Sold: 0</p>
-
 				</div>
 			</Card>
 
 			<div className="mt-4">
-                <Button className="mr-2">Edit Username</Button>
-                <Button className="mr-2">Edit Password</Button>
-                <Button className="mr-2">Edit Email</Button>
-            </div>
+				<Button className="mr-2">Edit Username</Button>
+				<Button className="mr-2">Edit Password</Button>
+				<Button className="mr-2">Edit Email</Button>
+			</div>
 
 			<Link to={"/logout"}>
 				<Button className="mt-4">Logout</Button>
 			</Link>
-			<p>Logged in as: {username}</p>
+			<p>Logged in as: {getUsername()}</p>
 		</div>
 	);
 };
