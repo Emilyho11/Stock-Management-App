9b3fa42e91a442270b7f4aee1afa3651d41fdb11
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-08-11 17:30:13 -0400

    Changing login page design

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,78 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
 import Button, { ButtonVariants } from "../components/Button";
 import AxiosClient from "../api/AxiosClient";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../components/AuthContext";
+import stocksBg from "../assets/images/bg.jpg";
 
 const Login = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [message, setMessage] = useState("");
-	const { login } = useAuth();
-	const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const { login } = useAuth();
+    const navigate = useNavigate();
 
-	const handleSubmit = async (event) => {
-		setMessage("Logging in...");
-		event.preventDefault();
-		try {
-			const response = await AxiosClient.post("/users/login", {
-				f_username: username,
-				f_password: password,
-			});
-			if (response.data.message === "Logged in") {
-				setMessage("Login successful");
-				login(username);
-				navigate("/");
-			} else {
-				setMessage("Login failed");
-			}
-		} catch (error) {
-			console.error("Error during login:", error);
-			setMessage("Error during login");
-		}
-	};
+    const handleSubmit = async (event) => {
+        setMessage("Logging in...");
+        event.preventDefault();
+        try {
+            const response = await AxiosClient.post("/users/login", {
+                f_username: username,
+                f_password: password,
+            });
+            if (response.data.message === "Logged in") {
+                setMessage("Login successful");
+                login(username);
+                navigate("/");
+            } else {
+                setMessage("Login failed");
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+            setMessage("Error during login");
+        }
+    };
 
-	return (
-		<div className="w-full">
-			<Card className="!w-full md:!w-1/3 p-4 ml-auto mr-auto flex flex-col ">
-				<h1>Login</h1>
-
-				<form
-					onSubmit={handleSubmit}
-					className="flex flex-col  w-full md:w-1/2 [&>input]:bg-gray-200 [&>input]:px-2 [&>input]:py-1 [&>input]:rounded-sm [&>label]:text-left"
-				>
-					<label htmlFor="username">Username:</label>
-					<input
-						type="text"
-						id="username"
-						name="username"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-					/>
-					<br className="my-2" />
-					<label htmlFor="password">Password:</label>
-					<input
-						type="password"
-						id="password"
-						name="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-					<br className="my-2" />
-					<Button type="submit" variant={ButtonVariants.LIGHT}>
-						Login{" "}
-					</Button>
-				</form>
-				{message && <p>{message}</p>}
-				<Link className="text-blue-600 hover:text-dark_red hover:underline" to="/signup">
-					Don't have an account yet? Sign up here.
-				</Link>
-			</Card>
-		</div>
-	);
+    return (
+        <div className="relative w-full h-screen flex items-center justify-center">
+			<div
+				className="absolute top-0 left-0 w-full h-full opacity-60 blur-sm grayscale-[40%]"
+				style={{
+					backgroundImage: `url(${stocksBg})`,
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+			></div>
+            <Card className="relative z-10 w-full md:w-1/4 px-14 flex flex-col ml-8 md:ml-32 bg-gray-100 py-24">
+                <h1>Login</h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col w-full [&>input]:bg-gray-200 [&>input]:px-2 [&>input]:py-2 [&>input]:rounded-sm [&>label]:text-left"
+                >
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+					<div className="my-4"></div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+					{message && <p>{message}</p>}
+					<Link className="text-blue-600 text-left hover:text-dark_red hover:underline my-6" to="/signup">
+						Don't have an account yet? Sign up here.
+					</Link>
+                    <Button type="submit" className="w-1/2 mx-auto flex items-center justify-center mt-6">
+                        Login
+                    </Button>
+                </form>
+            </Card>
+        </div>
+    );
 };
 
 export default Login;
