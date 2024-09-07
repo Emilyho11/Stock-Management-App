7c76b7c6982885e5ef07c77e2904560354e279ae
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-09-06 23:52:09 -0400

    Changing future stock calculation

--- stocks_api/src/main/java/stocks_api/stocks_api/routes/StockController.java
+++ stocks_api/src/main/java/stocks_api/stocks_api/routes/StockController.java
@@ -324,20 +324,58 @@ public class StockController {
     // For example, if the present value of a stock is $1000, the rate of interest is 10% and the time is 5 years,
     // the future value of the stock would be:
     // FV = 1000 * (1 + 0.1)^5 = 1000 * 1.61 = 1610
+//     @GetMapping("/future/{symbol}/{time}")
+//     @ResponseBody
+//     public ArrayList<Double> getFutureValue(@PathVariable String symbol,
+//                                             @PathVariable double time) {
+//         ArrayList<Double> futureValues = new ArrayList<>();
+//         StringBuilder sqlQuery = new StringBuilder("SELECT close FROM stock_data WHERE symbol = ?");
+//         // StringBuilder sqlQuery = new StringBuilder("SELECT close, low, high, open FROM stock_data WHERE symbol = ? LIMIT ?");
+//         String sqlQueryCOV = "SELECT cov FROM stocks WHERE symbol = ?";
+
+//         try (PreparedStatement preparedStatement = DBHandler.getInstance().getConnection().prepareStatement(sqlQuery.toString());
+//             PreparedStatement preparedStatementCOV = DBHandler.getInstance().getConnection().prepareStatement(sqlQueryCOV)) {
+
+//             preparedStatement.setString(1, symbol);
+//             // preparedStatement.setInt(2, (int) time);
+//             preparedStatementCOV.setString(1, symbol);
+
+//             try (ResultSet rs = preparedStatement.executeQuery();
+//                 ResultSet rsCOV = preparedStatementCOV.executeQuery()) {
+
+//                 if (rsCOV.next()) {
+//                     double cov = rsCOV.getDouble("cov");
+
+//                     while (rs.next()) {
+//                         double averagePrice = rs.getDouble("close");
+//                         double futureValue = averagePrice * Math.pow(1 + cov, time);
+//                         futureValues.add(futureValue);
+//                     }
+//                 }
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//             // Return an empty list instead of null to avoid potential NullPointerException
+//             return new ArrayList<>();
+//         }
+
+//         return futureValues;
+//     }
+// }
+
+
     @GetMapping("/future/{symbol}/{time}")
     @ResponseBody
     public ArrayList<Double> getFutureValue(@PathVariable String symbol,
                                             @PathVariable double time) {
-        ArrayList<Double> futureValues = new ArrayList<>();
-        StringBuilder sqlQuery = new StringBuilder("SELECT close FROM stock_data WHERE symbol = ?");
-        // StringBuilder sqlQuery = new StringBuilder("SELECT close, low, high, open FROM stock_data WHERE symbol = ? LIMIT ?");
-        String sqlQueryCOV = "SELECT cov FROM stocks WHERE symbol = ?";
 
+        ArrayList<Double> futureValues = new ArrayList<>();
+        StringBuilder sqlQuery = new StringBuilder("SELECT open, close FROM stock_data WHERE symbol = ?");
+        String sqlQueryCOV = "SELECT cov FROM stocks WHERE symbol = ?";
         try (PreparedStatement preparedStatement = DBHandler.getInstance().getConnection().prepareStatement(sqlQuery.toString());
             PreparedStatement preparedStatementCOV = DBHandler.getInstance().getConnection().prepareStatement(sqlQueryCOV)) {
 
             preparedStatement.setString(1, symbol);
-            // preparedStatement.setInt(2, (int) time);
             preparedStatementCOV.setString(1, symbol);
 
             try (ResultSet rs = preparedStatement.executeQuery();
@@ -345,12 +383,37 @@ public class StockController {
 
                 if (rsCOV.next()) {
                     double cov = rsCOV.getDouble("cov");
+                    double randomValue = 0;
+                    double futureValue = 0;
+                    double close = 0;
+                    double average = 0;
+                    double open = 0;
+                    ArrayList<Integer> randomList = new ArrayList<>();
 
                     while (rs.next()) {
-                        double averagePrice = rs.getDouble("close");
-                        // double averagePrice = (rs.getDouble("low") + rs.getDouble("high") + rs.getDouble("open") + rs.getDouble("close")) / 4.0;
-                        // Calculate future value using the growth rate (cov) and time
-                        double futureValue = averagePrice * Math.pow(1 + cov, time);
+                        open = rs.getDouble("open");
+                        close = rs.getDouble("close");
+                        average = (close - open) / 2;
+                        if (average < 0) {
+                            randomList.add(0);
+                        }
+                        else if (average == 0) {
+                            randomList.add(2);
+                        }
+                        else {
+                            randomList.add(1);
+                        }
+
+                        randomValue = randomList.get((int) (Math.random() * randomList.size()));
+                        if (randomValue == 0) {
+                            futureValue = close - (average * cov * time);
+                        }
+                        else if (randomValue == 2) {
+                            futureValue = close;
+                        }
+                        else {
+                            futureValue = close + (average * cov * time);
+                        }
                         futureValues.add(futureValue);
                     }
                 }
@@ -360,7 +423,6 @@ public class StockController {
             // Return an empty list instead of null to avoid potential NullPointerException
             return new ArrayList<>();
         }
-
         return futureValues;
     }
 }
